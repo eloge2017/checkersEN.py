36cf6cfb0a6c5d7f08134787dfed991923b412f1
eloge ndja <eloge.ndja@uamaker.nyc>
2019-03-05 21:17:53 -0500

    created a for loop for the J which creates 7 rows for 8 columns

--- checkersEN.py
+++ checkersEN.py
@@ -12,8 +12,9 @@ sqSz = 50
 chWin = GraphWin("Checkers", sqSz * 10, sqSz * 10)
 chWin.setCoords(0,0, sqSz * 10, sqSz * 10)
 
-for i in range (8):
-    draw_sq(sqSz * (i + 1), sqSz , sqSz, "red", chWin)
+for j in range (8):
+    for i in range (8):
+        draw_sq(sqSz * (i + 1), sqSz * (j + 1) , sqSz, "red", chWin)
 
 chWin.getMouse()
 chWin.close()
