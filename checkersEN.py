cd256c4eb943b3b78ea8bda4767536d619526328
eloge ndja <eloge.ndja@uamaker.nyc>
2019-03-05 21:11:29 -0500

    added the for loop which creates a row of squares

--- checkersEN.py
+++ checkersEN.py
@@ -2,14 +2,18 @@
 
 from graphics import*
 
+def draw_sq(sX, sY, size, color, win):
+    square = Rectangle(Point(sX, sY), Point(sX  + size, sY + size))
+    square.setFill(color)
+    square.draw(win)
+
 sqSz = 50
 
 chWin = GraphWin("Checkers", sqSz * 10, sqSz * 10)
 chWin.setCoords(0,0, sqSz * 10, sqSz * 10)
 
-sQ = Rectangle  (Point(sqSz , sqSz), Point(sqSz * 2, sqSz * 2))
-sQ.setFill("red")
-sQ.draw(chWin)
+for i in range (8):
+    draw_sq(sqSz * (i + 1), sqSz , sqSz, "red", chWin)
 
 chWin.getMouse()
 chWin.close()
